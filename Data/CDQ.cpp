8e9269ff92c52b49a917106573a15126b4bce602
xinkuleee <535062585@qq.com>
2024-05-07 19:19:02 +0800

    fixing Data

--- Data/CDQ.cpp
+++ Data/CDQ.cpp
@@ -2,25 +2,7 @@ int ans[maxn], lev[maxn];
 array<int, 5> v[maxn], tmp[maxn];
 
 struct BIT {
-    ll a[maxn];
-    int sz;
-    BIT(int x): sz(x) {};
-    BIT() {};
-    void resize(int x) {
-        sz = x;
-    }
-    ll query(int pos) {
-        ll res = 0;
-        for (int i = pos; i > 0; i -= (i & -i)) {
-            res += a[i];
-        }
-        return res;
-    }
-    void modify(int pos, ll d) {
-        for (int i = pos; i <= sz; i += (i & -i)) {
-            a[i] += d;
-        }
-    }
+	...
 } c;
 
 void solve(int l, int r) {
