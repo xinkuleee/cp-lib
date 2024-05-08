5a7e69e23d5776276ae223739e485bdd03c2fe5f
xinkuleee <535062585@qq.com>
2024-05-08 22:26:43 +0800

    fixing Data

--- Data/SparseTable.cpp
+++ Data/SparseTable.cpp
@@ -1,21 +1,26 @@
-template<class t1>
-struct ST {
-    int n;
-    static const int M = 21;
-    t1 p[M][maxn];
-    ST() {}
-    void build(t1 a[], int sz) {
-        n = sz;
-        rep(i, 1, n) p[0][i] = a[i];
-        rep(i, 1, M - 1)
-        rep(j, 1, n) if (j + bit(i) - 1 <= n) {
-            p[i][j] = max(p[i - 1][j], p[i - 1][j + bit(i - 1)]);
-        }
-    }
+template <typename T, class F = function<T(const T&, const T&)>>
+class SparseTable {
+public:
+	int n;
+	vector<vector<T>> mat;
+	F func;
 
-    t1 query(int l, int r) {
-        int len = r - l + 1;
-        int k = log2(len);
-        return max(p[k][l], p[k][r - bit(k) + 1]);
-    }
+	SparseTable(const vector<T>& a, const F& f) : func(f) {
+		n = static_cast<int>(a.size());
+		int max_log = 32 - __builtin_clz(n);
+		mat.resize(max_log);
+		mat[0] = a;
+		for (int j = 1; j < max_log; j++) {
+			mat[j].resize(n - (1 << j) + 1);
+			for (int i = 0; i <= n - (1 << j); i++) {
+				mat[j][i] = func(mat[j - 1][i], mat[j - 1][i + (1 << (j - 1))]);
+			}
+		}
+	}
+
+	T get(int from, int to) const {
+		assert(0 <= from && from <= to && to <= n - 1);
+		int lg = 32 - __builtin_clz(to - from + 1) - 1;
+		return func(mat[lg][from], mat[lg][to - (1 << lg) + 1]);
+	}
 };
