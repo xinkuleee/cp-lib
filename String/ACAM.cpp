dfbfa9d4fbe702808f2abcc3a4f603d9a632f0dd
xinkuleee <535062585@qq.com>
2024-07-28 13:41:30 +0800

    Add ACAM

--- String/ACAM.cpp
+++ String/ACAM.cpp
@@ -1,3 +1,43 @@
+const int AC_SIGMA = 26, AC_V = 26, AC_N = 810000;
+struct AC_automaton {
+	struct node {
+		node *go[AC_V], *fail, *f;
+// declare extra variables:
+	} pool[AC_N], *cur, *root, *q[AC_N];
+	node* newnode() {
+		node *p = cur++;
+// init extra variables:
+		return p;
+	}
+// CALL init() and CHECK all const variables:
+	void init() { cur = pool; root = newnode(); }
+	node* append(node *p, int w) {
+		if (!p->go[w]) p->go[w] = newnode(), p->go[w]->f = p;
+		return p->go[w];
+	}
+	void build() {
+		int t = 0;
+		q[t++] = root;
+		root->fail = root;
+		rep(i, 0, AC_SIGMA - 1) if (root->go[i]) {
+			q[t++] = root->go[i];
+			root->go[i]->fail = root;
+		} else {
+			root->go[i] = root;
+		}
+		rep(i, 1, t - 1) {
+			node *u = q[i];
+			rep(j, 0, AC_SIGMA - 1) if (u->go[j]) {
+				u->go[j]->fail = u->fail->go[j];
+				q[t++] = u->go[j];
+			} else {
+				u->go[j] = u->fail->go[j];
+			}
+		}
+	}
+} ac;
+typedef AC_automaton::node ACnode;
+
 const int M = 2, N = 2.1e5;
 struct node {
 	node *son[M], *go[M], *fail;
