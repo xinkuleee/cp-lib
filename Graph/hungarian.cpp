6b9cf0634964d5184068d2b535ca88bf9dfd16c7
xinkuleee <535062585@qq.com>
2024-10-15 23:17:00 +0800

    fix hungarian

--- Graph/hungarian.cpp
+++ Graph/hungarian.cpp
@@ -1,24 +1,23 @@
-vector<int> g[maxn];
-int idx;
-int a[N][N], use[N][N], p[maxn], vis[maxn];
-
-bool find(int x) {
-    vis[x] = 1;
-    for (auto y : g[x]) {
-        if (!p[y] || (!vis[p[y]] && find(p[y]))) {
-            p[y] = x;
-            return true;
-        }
-    }
-    return false;
+vector<vector<int>> e(SZ(rloc));
+vector<int> match(SZ(cloc), -1), vis(SZ(rloc));
+for (auto [u, v] : E) {
+  e[u].push_back(v);
 }
-
-int match() {
-    int res = 0;
-    fill(p + 1, p + idx + 1, 0);
-    for (int i = 1; i <= idx; i++) {
-        fill(vis + 1, vis + idx + 1, 0);
-        if (find(i)) res++;
+auto find = [&](auto&& find, int x) -> bool {
+  vis[x] = 1;
+  for (auto y : e[x]) {
+    if (match[y] == -1 || (!vis[match[y]] && find(find, match[y]))) {
+      match[y] = x;
+      return true;
     }
-    return res;
-}
+  }
+  return false;
+};
+auto DFSMatching = [&]() {
+  int res = 0;
+  rep(i, 0, SZ(rloc)) {
+    fill(all(vis), 0);
+    if (find(find, i)) res++;
+  }
+  return res;
+};
